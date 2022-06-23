2bd7ca1fcb4e1f6b790d09390ab5433c478dafd1
Philippe Desjardins <pdesjardins90@users.noreply.github.com>
2022-06-23 16:11:18 -0400

    x(dist): improved formula

--- distribution/formula.rb
+++ distribution/formula.rb
@@ -1,13 +1,12 @@
 # frozen_string_literal: true
 
 class ${PASCAL_CASED_PACKAGE} < Formula
-  desc "Software engineering task management CLI"
+  desc "Software engineering workflow automation CLI"
   homepage "https://github.com/logisparte/kano"
   url "https://github.com/logisparte/kano/releases/download/${RELEASE}/kano.tar.gz"
   version "${VERSION}"
   sha256 "${CHECKSUM}"
   license "GPL-3.0-only"
-  depends_on "docker" => :optional
   depends_on "git" => :optional
 
   def install
@@ -15,6 +14,18 @@ class ${PASCAL_CASED_PACKAGE} < Formula
     doc.install Dir["share/doc/*"]
     prefix.install "LICENSE"
     bin.write_exec_script (libexec/"kano")
+  end
+
+  def caveats
+    <<~EOS
+      Homebrew formula does not support this syntax:
+        depends_on cask: "docker" => :optional
+
+      So, optionally do:
+        brew install --cask docker
+
+      To install Docker Desktop
+    EOS
   end
 
   test do
